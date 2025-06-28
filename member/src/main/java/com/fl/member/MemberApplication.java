546c770d093cb97ca74b908f12c5fd3c1ea75d3a
Mr Big <fulurjj@gmail.com>
2025-06-28 22:10:19 +0800

    fix:修改类路径

--- member/src/main/java/com/fl/member/MemberApplication.java
+++ member/src/main/java/com/fl/member/MemberApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-import java.math.BigDecimal;
-
 /**
  * 启动类
  *
--- member/src/main/java/com/fl/member/interfaces/TestController.java
+++ member/src/main/java/com/fl/member/interfaces/TestController.java
@@ -1,4 +1,4 @@
-package com.fl.member.controller;
+package com.fl.member.interfaces;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
