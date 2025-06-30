f10c9804837032190380321fbbea3cbf6328c710
Mr Big <fulurjj@gmail.com>
2025-06-30 21:23:56 +0800

    feat:新增网关

---
+++ gateway/src/main/java/com/fl/train/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.fl.train.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * 启动类
+ *
+ * @author <a href="mailto:fulurjj@gmail.com">FuLu</a >
+ * @since 2025/1/15 23:28
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    private static final Logger logger =  LoggerFactory.getLogger(GatewayApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableEnvironment env = SpringApplication.run(GatewayApplication.class, args).getEnvironment();
+        logger.info("member start success，\"http://localhost:{}{}/hello\"", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+}
