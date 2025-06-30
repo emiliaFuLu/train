b08cd3202a6f1dc814d0fed4048d510c34401be3
Mr Big <fulurjj@gmail.com>
2025-06-30 21:08:59 +0800

    feat:添加基础设施

--- member/src/main/java/com/fl/train/member/adapter/web/TestController.java
+++ member/src/main/java/com/fl/train/member/adapter/web/TestController.java
@@ -1,4 +1,4 @@
-package com.fl.train.member.interfaces;
+package com.fl.train.member.adapter.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ member/src/main/java/com/fl/train/member/domain/entity/User.java
@@ -0,0 +1,9 @@
+package com.fl.train.member.domain.entity;
+
+/**
+ * @author <a href="mailto:fulurjj@gmail.com">FuLu</a >
+ * @since 2025/6/30 21:07
+ */
+public class User {
+
+}
